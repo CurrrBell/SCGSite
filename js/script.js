499dfbc3e23f5a87808245ed2e38266a7e16fcf2
CurrrBell <chrislbellny@gmail.com>
2016-10-17 16:20:08 -0400

    Made slider for portfolio.html
    Added Slick, a responsive carousel, to portfolio. Added lots of
    breakpoints for responsiveness and to switch to vertical on mobile
    devices.

--- js/script.js
+++ js/script.js
@@ -6,6 +6,44 @@ $(document).ready(function() {
     $('#myCarousel').on('slid.bs.carousel', function() {
     	//alert("slid");
 	});
+});
 
+$(document).ready(function(){
+	$('.demo').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		centerMode: true,
+		prevArrow: null,
+		nextArrow: null,
+		dots: true,
+		dotsClass: 'slick-dots',
+		pauseOnHover: true,
+		pauseOnDotsHover: true,
 
+		responsive: [{
+
+			breakpoint: 1350,
+			settings:{
+				slidesToShow: 3
+			}
+		},{
+
+			breakpoint: 1000,
+			settings:{
+				slidesToShow: 2
+			}
+		},{
+
+			breakpoint: 767,
+			settings:{
+				vertical: true,
+				verticalSwiping: true,
+				swipeToSlide: true,
+				dots:false
+			}
+		}]
+	});
 });
