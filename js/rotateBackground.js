73503076a83d51cbcbec2e7f9e21b2aee0160a6b
CurrrBell <chrislbellny@gmail.com>
2016-10-26 12:46:54 -0400

    WAIT THIS ACTUALLY WORKS?!
    fixed the index background...?

--- js/rotateBackground.js
+++ js/rotateBackground.js
@@ -1,10 +1,7 @@
 //script for transitioning background images
 
   var bgImageArray = document.getElementsByClassName("bg-image"),
-  base = "../images/backgrounds/",   //root folder of images
   secs = 10;    //time between images
-
-  var main = document.getElementById("main");
 
   function backgroundSequence() {
     var k = 0;
@@ -24,5 +21,29 @@
       }, (secs * 1000) * i)
     }
   }
+
+  var mobilebgImageArray = document.getElementsByClassName("mobile-bg-image"),
+  secs = 10;    //time between images
+
+
+  function mobileBackgroundSequence() {
+    var k = 0;
+    for (i = 0; i < bgImageArray.length; i++) {
+      setTimeout(function(){
+        mobilebgImageArray[k].className = "mobile-bg-image mobile-bg-hidden";
+        if(k+1 == bgImageArray.length){
+          mobilebgImageArray[0].className = "mobile-bg-image";
+        }else{
+          mobilebgImageArray[k+1].className = "mobile-bg-image";
+        }
+
+
+      if ((k + 1) === bgImageArray.length) {
+        setTimeout(function() { backgroundSequence() }, (secs * 1000))
+      } else { k++; }
+      }, (secs * 1000) * i)
+    }
+  }
   //backgroundSequence();
   setTimeout(function(){backgroundSequence() }, (secs * 1000));
+  setTimeout(function(){mobileBackgroundSequence() }, (secs * 1000));
